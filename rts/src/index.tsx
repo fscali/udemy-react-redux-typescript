2a17256e25bab045ff5f57d957ad30c9df106b7a
fscali <francesco.scali@gmail.com>
2021-08-03 15:10:16 +0200

    Typescript and refs

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -2,13 +2,16 @@ import ReactDOM from 'react-dom'
 import GuestList from './state/GuestList'
 import UserSearch from './state/UserSearch'
 import EventComponent from './events/EventComponent'
+import UserSearchWithRefs from './refs/UserSearch'
 const App = () => {
     return (
         <div>
             {/*             <GuestList />
              */}{' '}
             {/*  <UserSearch />{' '} */}
-            <EventComponent />
+            {/*             <EventComponent />
+             */}
+            <UserSearchWithRefs />{' '}
         </div>
     )
 }
