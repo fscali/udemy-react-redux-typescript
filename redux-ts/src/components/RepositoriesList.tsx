ac18a50ef8fb9953155d3d0fecdff55edfa4ae20
fscali <francesco.scali@gmail.com>
2021-08-04 11:09:13 +0200

    Typescript and redux: first version of component invoking action creator

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,13 +1,23 @@
+import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { actionCreators } from '../state'
 
 const RepositoriesList: React.FC = () => {
-  return (
-    <div>
-      <form>
-        <input />
-        <button>Search</button> 
-      </form>
-    </div>
-  )
+    const dispatch = useDispatch()
+    const [term, setTerm] = useState('')
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        dispatch(actionCreators.searchRepositories(term))
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={term} onChange={(e) => setTerm(e.target.value)} />
+                <button>Search</button>
+            </form>
+        </div>
+    )
 }
 
 export default RepositoriesList
