11fc67174b3e33351e84b811883f95d1802fa6e7
fscali <francesco.scali@gmail.com>
2021-08-07 16:17:29 +0200

    jbook - added some configurations to MonacoEditor to save space in the screen and to facilitate its usage for our scope

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,6 +1,22 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor language="javascript" theme="dark" height="500px" />;
+  return (
+    <MonacoEditor
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: true,
+        automaticLayout: true,
+      }}
+      language="javascript"
+      theme="dark"
+      height="500px"
+    />
+  );
 };
 export default CodeEditor;
