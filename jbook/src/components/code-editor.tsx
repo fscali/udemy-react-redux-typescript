6f5865e34fde58072c939a9fe51a46fc9120de1c
fscali <francesco.scali@gmail.com>
2021-08-07 22:23:04 +0200

    jbook - using prettier to beautify user code

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,4 +1,8 @@
+import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { editor } from 'monaco-editor';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,30 +10,50 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<editor.IStandaloneCodeEditor>();
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
+
+  const onFormatClick = () => {
+    // get current value
+    const unformatted = editorRef.current?.getModel()?.getValue();
+    // format current value
+    const formatted = prettier.format(unformatted!, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    // set formatted value in the editor
+    editorRef.current?.setValue(formatted);
+  };
   return (
-    <MonacoEditor
-      editorDidMount={onEditorDidMount}
-      value={initialValue} // NB: this is only the initial value
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: true,
-        automaticLayout: true,
-      }}
-      language="javascript"
-      theme="dark"
-      height="500px"
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue} // NB: this is only the initial value
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: true,
+          automaticLayout: true,
+        }}
+        language="javascript"
+        theme="dark"
+        height="500px"
+      />
+    </div>
   );
 };
 export default CodeEditor;
