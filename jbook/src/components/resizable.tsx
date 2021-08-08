62c0edf3eb3569cf674471ef5e5d1bc70702a59e
fscali <francesco.scali@gmail.com>
2021-08-08 23:12:16 +0200

    jbook - Resizable modified to manage both horizontal and vertical - still something to do for horizontal though..

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -6,17 +6,25 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      minConstraints={[Infinity, 24]}
-      resizeHandles={['s']}
-      height={300}
-      width={Infinity}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+  if (direction === 'horizontal') {
+    resizableProps = {
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      resizeHandles: ['e'],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+    };
+  } else {
+    resizableProps = {
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, 24],
+      resizeHandles: ['s'],
+      height: 300,
+      width: Infinity,
+    };
+  }
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
