1d5fd820f39bd46c8787cb6206bd0148016be9bb
fscali <francesco.scali@gmail.com>
2021-08-10 13:06:59 +0200

    jbook - cellsReducer skeleton

---
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -0,0 +1,34 @@
+import { Action } from '../actions';
+import { ActionType } from '../action-types';
+import { Cell } from '../Cell';
+
+interface CellsState {
+  data: { [key: string]: Cell };
+  loading: boolean;
+  error: string | null;
+  order: string[];
+}
+
+const initialState: CellsState = {
+  data: {},
+  loading: false,
+  error: null,
+  order: [],
+};
+
+const cellsReducer = (state = initialState, action: Action): CellsState => {
+  switch (action.type) {
+    case ActionType.UPDATE_CELL:
+      return state;
+    case ActionType.DELETE_CELL:
+      return state;
+    case ActionType.MOVE_CELL:
+      return state;
+    case ActionType.INSERT_CELL_BEFORE:
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default cellsReducer;
