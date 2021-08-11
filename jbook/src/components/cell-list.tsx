dc70a06c1d3d2e711380bb4f34dfdcb60f13f679
fscali <francesco.scali@gmail.com>
2021-08-11 11:39:03 +0200

    jbook - redux - accessing the cell list with a typedSelector hook

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,4 +1,8 @@
+import { useStore } from 'react-redux';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 const CellList: React.FC = () => {
+  const cells = useTypedSelector((state) => state.cells);
+  console.log('Cells', cells);
   return <div>Cell List</div>;
 };
 export default CellList;
