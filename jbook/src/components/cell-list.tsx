e3c87011237c5f8c4e60ad69a57a75a2173f353b
fscali <francesco.scali@gmail.com>
2021-08-11 11:43:38 +0200

    jbook - redux - as previous commit with a more sinthetic syntax

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,11 +1,9 @@
 import { useStore } from 'react-redux';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 const CellList: React.FC = () => {
-  const cells = useTypedSelector(({ cells: { order, data } }) => {
-    return order.map((id) => {
-      return data[id];
-    });
-  });
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
   console.log('Cells', cells);
   return <div>Cell List</div>;
 };
